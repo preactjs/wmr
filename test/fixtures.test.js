4a91e7162d8bfa792f6f72732e87a9abbdd5f69f
Jason Miller <jason@developit.ca>
2020-06-12 15:18:12 -0400

    Add test for empty HTML-only project

---
+++ test/fixtures.test.js
@@ -0,0 +1,31 @@
+import { setupTest, teardown, runWmr, loadFixture, waitForMessage } from './test-helpers';
+
+jest.setTimeout(30000);
+
+describe('fixtures', () => {
+	/** @type {TestEnv} */
+	let env;
+	/** @type {WmrInstance} */
+	let instance;
+
+	beforeEach(async () => {
+		env = await setupTest();
+	});
+
+	afterEach(async () => {
+		await teardown(env);
+		instance.close();
+	});
+
+	it('should listen on port', async () => {
+		await loadFixture('empty', env);
+		instance = await runWmr(env.tmp.path);
+		await waitForMessage(instance.output, /^Listening/);
+
+		const address = instance.output.join('\n').match(/https?:\/\/localhost:\d+/g)[0];
+
+		await env.page.goto(address);
+
+		expect(await env.page.content()).toMatch(`<h1>Hello wmr</h1>`);
+	});
+});
