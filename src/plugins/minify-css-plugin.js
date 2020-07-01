dd16d57aa40f53358bdb8ef8d451d4ce16fc4d06
Jason Miller <developit@users.noreply.github.com>
2020-07-01 23:15:12 +0000

    Consolidate CSS minification to allow cross-file transforms.

--- src/plugins/minify-css-plugin.js
+++ src/plugins/minify-css-plugin.js
@@ -1,16 +1,45 @@
+import { posix } from 'path';
 import cssnano from 'cssnano';
+import postcss from 'postcss';
 
-export default function cssEntriesPlugin() {
+const processor = postcss([cssnano()]);
+
+/** @returns {import('rollup').Plugin} */
+export default function minifyCssPlugin() {
 	return {
-		name: 'minify-styles',
-		async transform(code, id) {
-			if (!/.css$/.test(id) || /.module.css$/.test(id)) return;
-
-			const result = await cssnano.process(code, { from: id, to: id });
-			return {
-				code: result.css,
-				map: result.map || { mappings: '' }
-			};
+		name: 'minify-css',
+		async resolveId(id, importer) {
+			if (id.startsWith('css:')) {
+				const resolved = await this.resolve(id.substring(4), importer);
+				resolved.id = 'css:' + resolved.id;
+				return resolved;
+			}
+		},
+		async generateBundle(_, bundle) {
+			await Promise.all(
+				Object.values(bundle).map(async asset => {
+					if (asset.type !== 'asset' || !/\.css$/.test(asset.fileName)) return;
+					const id = asset.fileName;
+					const mapFile = asset.fileName + '.map';
+					const result = await processor.process(asset.source, {
+						from: id,
+						to: id,
+						map: {
+							annotation: posix.basename(mapFile),
+							from: posix.basename(mapFile),
+							sourcesContent: false
+						}
+					});
+					if (result.map) {
+						this.emitFile({
+							type: 'asset',
+							fileName: mapFile,
+							source: result.map.toString()
+						});
+					}
+					asset.source = result.css;
+				})
+			);
 		}
 	};
 }
