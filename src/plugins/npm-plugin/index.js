d3a3f3984191ea37e62d11f335c67012c71342a4
Jason Miller <jason@developit.ca>
2020-07-16 17:08:52 -0400

    allow dots in npm package names

--- src/plugins/npm-plugin/index.js
+++ src/plugins/npm-plugin/index.js
@@ -79,7 +79,7 @@ export default function npmPlugin({ publicPath = '/@npm', prefix = '\bnpm/', ext
 	};
 }
 
-const PACKAGE_SPECIFIER = /^((?:@[a-z0-9-]{1,200}\/)?[a-z0-9-]{1,200})(?:@([a-z0-9^.~>=<-]{1,50}))?(?:\/(.*))?$/i;
+const PACKAGE_SPECIFIER = /^((?:@[\w.-]{1,200}\/)?[\w.-]{1,200})(?:@([a-z0-9^.~>=<-]{1,50}))?(?:\/(.*))?$/i;
 
 export const normalizeSpecifier = memo(spec => {
 	let [, module = '', version = '', path = ''] = spec.match(PACKAGE_SPECIFIER) || [];
@@ -96,103 +96,3 @@ function isDiskPath(filename) {
 	if (sep === '\\' && /^(([A-Z]+:)?\\|\.\.?(\\|$))/.test(filename)) return true;
 	return /^(file:\/\/)?([A-Z]:)?(\/|\.\.?(\/|$))/.test(filename);
 }
-
-// /** @param {import('./registry.js').Module} spec */
-// async function resolveModule({ module, version, path }) {
-// 	const readFile = (path = '') => loadPackageFile({ module, version, path });
-// 	const hasFile = path =>
-// 		readFile(path)
-// 			.then(() => true)
-// 			.catch(() => false);
-
-// 	let pkg;
-// 	try {
-// 		pkg = JSON.parse(await readFile('package.json'));
-// 	} catch (e) {
-// 		throw Error(`Invalid package.json for ${module}: ${e.message}`);
-// 	}
-
-// 	// Package Export Maps
-// 	if (pkg.exports) {
-// 		const entry = path ? `./${path}` : '.';
-
-// 		const mapped = resolveExportMap(pkg.exports, entry, ENV_KEYS);
-
-// 		if (!mapped) {
-// 			throw Error(`Unknown package export ${entry} in ${module}.\n\n${JSON.stringify(pkg.exports, null, 2)}`);
-// 		}
-
-// 		// true means directory access was allowed for this entry, but it was not resolved.
-// 		if (mapped !== true) {
-// 			return mapped.replace(/^\./, '');
-// 		}
-// 	}
-
-// 	// path is a bare import of a package, use its legacy exports (module/main):
-// 	if (!path) {
-// 		return getLegacyEntry(pkg);
-// 	}
-
-// 	// fallback: implement basic commonjs-style resolution
-// 	if (/\.([mc]js|[tj]sx?)$/i.test(path)) {
-// 		return path;
-// 	}
-
-// 	// extensionless paths:
-// 	if (await hasFile(path + '.js')) {
-// 		return path + '.js';
-// 	}
-
-// 	// path is a directory, check for package.json:
-// 	try {
-// 		const subPkg = JSON.parse(await readFile(path + '/package.json'));
-// 		return path + getLegacyEntry(subPkg);
-// 	} catch (e) {}
-
-// 	// fall back to implicit directory /index.js:
-// 	if (await hasFile(path + '/index.js')) {
-// 		return path + '/index.js';
-// 	}
-
-// 	return path;
-// }
-
-// /** Get the best possible entry from a package.json that doesn't have an Export Map */
-// function getLegacyEntry(pkg) {
-// 	const entry = String(pkg.esmodules || pkg.modern || pkg.module || pkg['jsnext:main'] || pkg.browser || pkg.main);
-// 	return entry.replace(/^\/?/, '/');
-// }
-
-// const ENV_KEYS = ['esmodules', 'import', 'require', 'browser', 'node', 'default'];
-
-// /** Get the best resolution for an entry from an Export Map
-//  * @param {Object} exp `package.exports`
-//  * @param {string} entry `./foo` or `.`
-//  * @param {string[]} [envKeys=ENV_KEYS] package environment keys
-//  * @returns {string | boolean} a resolved path, or a boolean indicating if the given entry is exposed
-//  */
-// function resolveExportMap(exp, entry, envKeys) {
-// 	if (typeof exp === 'string') {
-// 		// {"exports":"./foo.js"}
-// 		// {"exports":{"./foo":"./foo.js"}}
-// 		return exp;
-// 	}
-// 	let isFileListing;
-// 	let isDirectoryExposed = false;
-// 	for (let i in exp) {
-// 		if (isFileListing === undefined) isFileListing = i[0] === '.';
-// 		if (isFileListing) {
-// 			// {"exports":{".":"./index.js"}}
-// 			if (i === entry) {
-// 				return resolveExportMap(exp[i], entry, envKeys);
-// 			}
-// 			if (!isDirectoryExposed && i.endsWith('/') && entry.startsWith(i)) {
-// 				isDirectoryExposed = true;
-// 			}
-// 		} else if (envKeys.includes(i)) {
-// 			// {"exports":{"import":"./foo.js"}}
-// 			return resolveExportMap(exp[i], entry, envKeys);
-// 		}
-// 	}
-// 	return isDirectoryExposed;
-// }
