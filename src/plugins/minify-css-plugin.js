5d438a038bd5879509242b8c6afdc9d3910aee88
Jason Miller <jason@developit.ca>
2020-10-09 16:23:14 -0400

    code frames for css minification errors

--- src/plugins/minify-css-plugin.js
+++ src/plugins/minify-css-plugin.js
@@ -25,15 +25,34 @@ export default function minifyCssPlugin({ sourcemap } = {}) {
 					if (asset.type !== 'asset' || !/\.(css|s[ac]ss)$/.test(asset.fileName)) return;
 					const id = asset.fileName;
 					const mapFile = asset.fileName + '.map';
-					const result = await processor.process(asset.source, {
-						from: id,
-						to: id,
-						map: sourcemap && {
-							annotation: posix.basename(mapFile),
-							from: posix.basename(mapFile),
-							sourcesContent: false
+					let result;
+					try {
+						result = await processor.process(asset.source, {
+							from: id,
+							to: id,
+							map: sourcemap && {
+								annotation: posix.basename(mapFile),
+								from: posix.basename(mapFile),
+								sourcesContent: false
+							}
+						});
+					} catch (e) {
+						if (e.line != null && e.column != null) {
+							const lines = e.source.split('\n');
+							const line = e.line - 1;
+							const frame = [
+								lines[line - 2] || '',
+								lines[line - 1] || '',
+								lines[line],
+								'-'.repeat(e.column - 1) + '^',
+								lines[line + 1] || '',
+								lines[line + 2] || ''
+							];
+							const err = Error(e.message + '\n> ' + frame.join('\n> '));
+							return this.error(err, { line: e.line, column: e.column });
 						}
-					});
+						throw e;
+					}
 					if (result.map) {
 						this.emitFile({
 							type: 'asset',
