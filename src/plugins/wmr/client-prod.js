1dce82ea3ba6c32b97dc75750a705132e64c5451
Jason Miller <jason@developit.ca>
2020-11-03 11:00:56 -0500

    Make style() importer return a Promise

--- src/plugins/wmr/client-prod.js
+++ src/plugins/wmr/client-prod.js
@@ -2,9 +2,13 @@ export function createHotContext() {}
 
 export function style(filename) {
 	const prev = document.querySelector('link[rel=stylesheet][href="' + filename + '"]');
+	if (prev) return Promise.resolve();
 	if (prev) return;
 	const node = document.createElement('link');
 	node.rel = 'stylesheet';
 	node.href = filename;
 	document.head.appendChild(node);
+	return new Promise(resolve => {
+		node.onload = node.onerror = resolve;
+	});
 }
