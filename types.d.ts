02aadfbab01dff218c422338beafaaa2f407c442
Jason Miller <jason@developit.ca>
2020-11-28 22:30:47 -0500

    include types for non-modular CSS

--- types.d.ts
+++ types.d.ts
@@ -40,10 +40,13 @@ declare interface NodeModule {
 }
 declare var module: NodeModule;
 
+declare module '*.css' { const url: string; export default url; }
+declare module '*.scss' { const url: string; export default url; }
+declare module '*.sass' { const url: string; export default url; }
+declare module '*.styl' { const url: string; export default url; }
+
 /** Maps authored classNames to their CSS Modules -suffixed generated classNames. */
-interface Mapping {
-	[key: string]: string;
-}
+interface Mapping { [key: string]: string; }
 declare module '*.module.css' { const mapping: Mapping; export default mapping; }
 declare module '*.module.scss' { const mapping: Mapping; export default mapping; }
 declare module '*.module.sass' { const mapping: Mapping; export default mapping; }
