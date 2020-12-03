6b25180b870aab137bb29f7f9cc8295d9f0067c3
Jovi De Croock <decroockjovi@gmail.com>
2020-12-03 21:02:37 +0100

    chore: add preact-iso (#217)
    * add preact-iso
    
    * move out of src folder for Node 12

---
+++ packages/preact-iso/index.js
@@ -0,0 +1,7 @@
+export { Router, LocationProvider, useLoc } from './router.js';
+export { default as lazy, ErrorBoundary }  from './lazy.js';
+export { default as hydrate } from './hydrate.js';
+
+export function prerender(vnode, options) {
+	return import('./prerender.js').then(m => m.default(vnode, options));
+}
