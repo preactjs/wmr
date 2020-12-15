792b9e8efc76f0226cbf79ee2509cbe0f644ea16
Ryan Christian <ryanchristian4427@gmail.com>
2020-12-15 11:33:05 -0600

    fix: PR comments
    Co-authored-by: Marvin Hagemeister <hello@marvinh.dev>

--- packages/preact-iso/router.d.ts
+++ packages/preact-iso/router.d.ts
@@ -4,7 +4,7 @@ export const LocationProvider: FunctionComponent;
 
 export function Router(props: { onLoadEnd?: () => void, onLoadStart?: () => void, children?: VNode[] }): VNode;
 
-export const useLoc: () => { url: string, path: string, query: Record<string, string>, route };
+export const useLoc: () => { url: string, path: string, query: Record<string, string>, route: (url: string) => void };
 
 interface RoutableProps {
 	path?: string;
