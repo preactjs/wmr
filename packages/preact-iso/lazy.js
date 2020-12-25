773b3996fd072d1b5c5bc86d9048eccf13dfadbe
Jason Miller <developit@users.noreply.github.com>
2020-12-25 07:41:38 -0500

    [preact-iso] Make lazy() thrown promise resolve to component (#281)
    Fixes #280.

--- packages/preact-iso/lazy.js
+++ packages/preact-iso/lazy.js
@@ -4,10 +4,10 @@ import { useState, useRef } from 'preact/hooks';
 export default function lazy(load) {
 	let p, c;
 	return props => {
-		if (!p) p = load().then(m => ((c = (m && m.default) || m), 1));
+		if (!p) p = load().then(m => c = (m && m.default) || m);
 		const [, update] = useState(0);
 		const r = useRef(c);
-		if (!r.current) r.current = p.then(update);
+		if (!r.current) r.current = p.then(() => update(1));
 		if (c === undefined) throw p;
 		return h(c, props);
 	};
