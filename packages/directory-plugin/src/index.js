2625a1cd588e04911f50a5ff5db8cb1fb3d3c2b3
jdecroock <decroockjovi@gmail.com>
2021-02-20 12:43:14 +0100

    make lines consistent in source

--- packages/directory-plugin/src/index.js
+++ packages/directory-plugin/src/index.js
@@ -9,32 +9,32 @@ const pathToPosix = p => p.split(path.sep).join(path.posix.sep);
  * @returns {import('rollup').Plugin}
  */
 function directoryPlugin({ cwd } = {}) {
-  return {
-    name: 'directory',
-    async resolveId(id, importer) {
+	return {
+		name: 'directory',
+		async resolveId(id, importer) {
 			if (!id.startsWith('dir:')) return;
 
-      const resolved = await this.resolve(id.slice(4) + '\0', importer, { skipSelf: true });
+			const resolved = await this.resolve(id.slice(4) + '\0', importer, { skipSelf: true });
 
-      if (resolved) {
+			if (resolved) {
 				return '\0dir:' + pathToPosix(resolved.id).replace(/\0$/, '');
 			}
-    },
-    async load(id) {
-      if (!id.startsWith('\0dir:')) return;
+		},
+		async load(id) {
+			if (!id.startsWith('\0dir:')) return;
 
-      // remove the "\dir:" prefix and convert to an absolute path:
-      id = path.resolve(cwd || '.', id.slice(5));
+			// remove the "\dir:" prefix and convert to an absolute path:
+			id = path.resolve(cwd || '.', id.slice(5));
 
-      // watch the directory for changes:
-      this.addWatchFile(id);
+			// watch the directory for changes:
+			this.addWatchFile(id);
 
 			// generate a module that exports the directory contents as an Array:
 			const files = (await fs.readdir(id)).filter(d => d[0] != '.');
 
 			return `export default ${JSON.stringify(files)}`;
-    }
-  };
+		}
+	};
 }
 
 export default directoryPlugin;
