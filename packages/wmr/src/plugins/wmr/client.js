d7612ad1e59499a39053198d9e88feac438d7c48
Jovi De Croock <decroockjovi@gmail.com>
2021-02-26 09:22:29 +0100

    Update packages/wmr/src/plugins/wmr/client.js
    Co-authored-by: Jason Miller <developit@users.noreply.github.com>

--- packages/wmr/src/plugins/wmr/client.js
+++ packages/wmr/src/plugins/wmr/client.js
@@ -40,7 +40,7 @@ function handleMessage(e) {
 				url = resolve(url);
 
 				if (!mods.get(url)) {
-					if (/\.css$/.test(url) || /\.s[ac]ss$/.test(url)) {
+					if (/\.(css|s[ac]ss)$/.test(url)) {
 						if (mods.has(url + '.js')) {
 							url += '.js';
 						} else {
