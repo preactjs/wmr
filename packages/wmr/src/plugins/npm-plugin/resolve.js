70ff5f4a0705da00921597966a5d0b74e5e35ddf
Jovi De Croock <decroockjovi@gmail.com>
2021-03-01 22:38:12 +0100

    Update packages/wmr/src/plugins/npm-plugin/resolve.js
    Co-authored-by: Luke Edwards <luke.edwards05@gmail.com>

--- packages/wmr/src/plugins/npm-plugin/resolve.js
+++ packages/wmr/src/plugins/npm-plugin/resolve.js
@@ -12,7 +12,7 @@ function resolveExports(pkg, key) {
 function resolveLegacyEntry(pkg, path) {
 	_resolveLegacyEntry(pkg, {
 		browser: path,
-		fields: ['esmodules', 'modern', 'module', 'jsnext:main']
+		fields: ['esmodules', 'modern', 'module', 'jsnext:main', 'browser', 'main']
 	}) || 'index.js';
 }
 
