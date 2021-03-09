7d115cd6eb5d8878e6196e216ec9e64e9a3ec244
Jovi De Croock <decroockjovi@gmail.com>
2021-03-09 19:37:00 +0100

    fix test arguments

--- packages/preact-iso/test/match.test.js
+++ packages/preact-iso/test/match.test.js
@@ -26,7 +26,7 @@ describe('match', () => {
 	});
 
 	it('Handles leading/trailing slashes', () => {
-		const result = exec('/about-late/_SEGMENT1_/_SEGMENT2_/', '/about-late/:seg1/:seg2/');
+		const result = exec('/about-late/_SEGMENT1_/_SEGMENT2_/', '/about-late/:seg1/:seg2/', {});
 		expect(result).toEqual({
 			seg1: '_SEGMENT1_',
 			seg2: '_SEGMENT2_'
