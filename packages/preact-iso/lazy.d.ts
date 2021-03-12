3db36964afc1f3158d3b5d377d06ccf32b6efadd
Marvin Hagemeister <hello@marvinh.dev>
2021-03-12 08:28:49 +0100

    Add missing `onError` type for ErrorBoundary

--- packages/preact-iso/lazy.d.ts
+++ packages/preact-iso/lazy.d.ts
@@ -2,4 +2,4 @@ import { ComponentChildren, VNode } from 'preact';
 
 export default function lazy<T>(load: () => Promise<{ default: T } | T>): T;
 
-export function ErrorBoundary(props: { children?: ComponentChildren }): VNode;
+export function ErrorBoundary(props: { children?: ComponentChildren; onError?: (error: Error) => void }): VNode;
