5ae4e4dc1c7553ed64d7dbbe7a05281e215d64b2
jdecroock <decroockjovi@gmail.com>
2021-03-21 11:48:16 +0100

    add missing import prefix types

--- packages/sw-plugin/types.d.ts
+++ packages/sw-plugin/types.d.ts
@@ -2,3 +2,8 @@ import { Plugin } from 'rollup';
 import { Options } from 'wmr';
 
 export default function swPlugin(options?: Options): Plugin;
+
+declare module 'sw:*' {
+	const url: string;
+	export default url;
+}
