56de2c7f9aa91d6fbfc1c4e0d638cf678839fcc5
Marvin Hagemeister <hello@marvinh.dev>
2021-03-31 00:41:06 +0200

    Add feature list to home page

--- docs/public/components/home-page.js
+++ docs/public/components/home-page.js
@@ -4,16 +4,55 @@ import Jumbotron from './jumbotron.js';
 import { useContent } from '../lib/use-content.js';
 import homeContent from 'markdown:../content/index.md';
 
+/**
+ *
+ * @param {{title: string, children: string}} props
+ * @returns
+ */
+function Feature(props) {
+	return (
+		<div class="feature">
+			<h3 class="feature-title">{props.title}</h3>
+			<p>{props.children}</p>
+		</div>
+	);
+}
+
 export default function Home() {
 	const { html, meta } = useContent(homeContent);
 
 	return (
-		<section class="fullwidth">
+		<div>
 			<Meta {...meta} />
 			<Jumbotron />
-			<div class="md">
-				<Markup html={html} />
+			<div class="feature-grid">
+				<Feature title="Instant Server Startup">
+					Fire up any project and start working immediately. The server is ready to respond in the blink of an eye!
+				</Feature>
+				<Feature title="Snappy HMR">
+					Hot module reloading (=HMR) for modules, CSS and frameworks that stays fast, no matter how big your may
+					project grow.
+				</Feature>
+				<Feature title="No entrys to configure">
+					Just point WMR to HTML files and we'll do the rest.WMR automatically compiles any linked scripts or referenced
+					assets.
+				</Feature>
+				<Feature title="Builtin TypeScript Support">
+					Just point WMR to HTML files and we'll automatically compile any linked scripts.
+				</Feature>
+				<Feature title="Optimized production output">
+					Just point WMR to HTML files and we'll automatically compile any linked scripts.
+				</Feature>
+				<Feature title="Rollup compatible plugin API">
+					Just point WMR to HTML files and we'll automatically compile any linked scripts.
+				</Feature>
 			</div>
-		</section>
+			<div class="md">{/* <Markup html={html} /> */}</div>
+			<div class="container">
+				<code>
+					npm init create-wmr my-project <button>clip</button>
+				</code>
+			</div>
+		</div>
 	);
 }
--- docs/public/components/jumbotron.js
+++ docs/public/components/jumbotron.js
@@ -6,7 +6,10 @@ export default function Jumbotron() {
 				<img src="/assets/wmr-shadow.svg" class="brand-shadow" alt="wmr logo" width="300" height="300" />
 				<h1>WMR</h1>
 			</div>
-			<h2 class="tagline">The tiny all-in-one development tool for modern web apps.</h2>
+			<h2 class="tagline">
+				The modern web app development tool for developers who don't like waiting for file changes to show up in the
+				browser.
+			</h2>
 			<div class="cta">
 				<a href="/docs" class="btn btn-primary">
 					Get started
@@ -14,11 +17,6 @@ export default function Jumbotron() {
 				<a href="https://github.com/preactjs/wmr" target="_blank" rel="noopener noreferrer" class="btn btn-secondary">
 					<img class="btn-img-left icon" src="/assets/github.svg" alt="" width="20" height="20" /> GitHub
 				</a>
-			</div>
-			<div>
-				<code>
-					npm init create-wmr my-project <button>clip</button>
-				</code>
 			</div>
 		</div>
 	);
