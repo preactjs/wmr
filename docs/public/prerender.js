e78a4b54a00858d14a31d8839cbf44b22aa58cd4
Jason Miller <jason@developit.ca>
2021-03-29 23:20:26 -0400

    node 12

--- docs/public/prerender.js
+++ docs/public/prerender.js
@@ -3,7 +3,7 @@ import render from 'preact-iso/prerender';
 let initialized = false;
 // install a fetch+DOMParser "polyfills" for Node (used by content & <Markup>)
 async function init() {
-	const fs = await eval('u=>import(u)')('fs/promises');
+	const fs = (await eval('u=>import(u)')('fs')).promises;
 	globalThis.fetch = async url => {
 		const text = () => fs.readFile('dist/' + String(url).replace(/^\//, ''), 'utf-8');
 		return { text, json: () => text().then(JSON.parse) };
