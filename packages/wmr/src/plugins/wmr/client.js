4d5251cfcc4e1ba25f302881f3f892638ea14de8
Marvin Hagemeister <hello@marvinh.dev>
2021-04-25 19:00:42 +0200

    Render error overlay into iframe to scope styles

--- packages/wmr/src/plugins/wmr/client.js
+++ packages/wmr/src/plugins/wmr/client.js
@@ -237,34 +237,45 @@ function updateStyleSheet(url) {
 	}
 }
 
-/**
- * @type {<K extends keyof HTMLElementTagNameMap>(tag: K, attrs: any) => HTMLElementTagNameMap[K]}
- */
-function createDom(tag, attrs) {
-	const el = document.createElement(tag);
-	for (const attr in attrs) {
-		const value = attrs[attr];
-		if (typeof value === 'function') {
-			el.addEventListener(attr.slice(2), value);
-		} else {
-			el.setAttribute(attr, value);
-		}
+// Listen for iframe close events
+window.addEventListener('message', message => {
+	if (message.data === 'wmr-close-error-overlay') {
+		document.getElementById('wmr-error-overlay')?.remove();
 	}
-	return el;
-}
+});
 
 /**
  *
  * @param {{type: "error", error: string, codeFrame: string, stack: import('errorstacks').StackFrame[]}} data
  */
 function createErrorOverlay(data) {
-	const existing = document.getElementById('wmr-error-overlay');
-	if (existing) existing.remove();
+	document.getElementById('wmr-error-overlay')?.remove();
 
-	const outer = createDom('div', { id: 'wmr-error-overlay' });
-	const style = document.createElement('style');
-	style.textContent = `
-		#wmr-error-overlay {
+	const iframe = document.createElement('iframe');
+	iframe.id = 'wmr-error-overlay';
+	iframe.setAttribute(
+		'style',
+		`position: fixed; top: 0; left: 0; bottom: 0; right: 0; z-index: 99999; width: 100%; height: 100%; border: none;`
+	);
+
+	iframe.addEventListener('load', () => {
+		function createDom(tag, attrs) {
+			const el = iframe.contentDocument?.createElement(tag);
+			for (const attr in attrs) {
+				const value = attrs[attr];
+				if (typeof value === 'function') {
+					el.addEventListener(attr.slice(2), value);
+				} else {
+					el.setAttribute(attr, value);
+				}
+			}
+			return el;
+		}
+
+		const outer = createDom('div', { id: 'wmr-error-overlay' });
+		const style = document.createElement('style');
+		style.textContent = `
+		:root {
 			--bg: #fff;
 			--bg-code-frame: rgb(255, 0, 32, 0.1);
 			--bg-active-line: #fbcecc;
@@ -274,13 +285,16 @@ function createErrorOverlay(data) {
 			--code: #333;
 			font-family: sans-serif;
 			line-height: 1.4;
+			color: var(--text);
+			background: var(--bg);
 		}
-		#wmr-error-overlay * {
+
+		* {
 			box-sizing: border-box;
 		}
 		
 		@media (prefers-color-scheme: dark) {
-			#wmr-error-overlay {
+			:root {
 				--bg-code-frame: rgba(251, 93, 113, 0.2);
 				--bg-active-line: #4f1919;
 				--bg: #353535;
@@ -290,113 +304,105 @@ function createErrorOverlay(data) {
 			}
 		}
 
-		.wmr-error-container {
-			position: fixed;
-			top: 0;
-			left: 0;
-			right: 0;
-			bottom: 0;
-			z-index: 9999;
-			color: var(--text);
-			background: var(--bg);
-		}
-
-		.wmr-error-close {
+		.close {
 			cursor: pointer;
 			position: absolute;
 			top: 1rem;
 			right: 1rem;
 		}
 
-		.wmr-error-inner {
+		.inner {
 			max-width: 80ch;
 			padding: 4rem 1rem;
 			margin: 0 auto;
 		}
 
-		.wmr-error-title {
+		.title {
 			color: var(--title);
 			font-weight: normal;
 			font-size: 1.5rem;
 		}
 
-		.wmr-error-code-frame {
+		.code-frame {
 			overflow: auto;
 			padding: 0.5rem;
 			background: var(--bg-code-frame);
 			color: var(--code);
 		}
-		.wmr-error-line {
+		.line {
 			padding: 0.25rem 0.5rem;
 		}
-		.wmr-error-active-line {
+		.active-line {
 			display: inline-block;
 			width: 100%;
 			background: var(--bg-active-line);
 		}
 
-		.wmr-error-detail {
+		.detail {
 			cursor: pointer;
 			color: var(--text2);
 		}
-		.wmr-error-stack-frame {
+		.stack-frame {
 			padding: 0.5rem 0;
 		}
-		.wmr-error-stack-name {
+		.stack-name {
 			color: var(--text);
 		}
-		.wmr-error-stack-loc {
+		.stack-loc {
 			color: var(--text2);
 			font-family: monospace;
 		}
 	`;
-	const overlay = createDom('div', { class: 'wmr-error-container' });
-	const close = createDom('button', {
-		class: 'wmr-error-close',
-		onclick: () => {
-			const dom = document.getElementById('wmr-error-overlay');
-			if (dom) dom.remove();
-		}
+		const overlay = createDom('div', {});
+		const close = createDom('button', {
+			class: 'close',
+			onclick: () => {
+				window.postMessage('wmr-close-error-overlay', '*');
+			}
+		});
+		close.textContent = 'close';
+		const inner = createDom('div', { class: 'inner' });
+		overlay.append(close, inner);
+
+		const title = createDom('h1', { class: 'title' });
+		title.textContent = data.error;
+		inner.append(title);
+
+		const codeFrame = createDom('pre', { class: 'code-frame' });
+		const code = createDom('code', {});
+		codeFrame.append(code);
+
+		data.codeFrame.split('\n').forEach((line, i, arr) => {
+			const dom = createDom('span', {
+				class: 'line' + (line.startsWith('>') ? ' active-line' : '')
+			});
+			dom.textContent = line;
+			code.append(dom, i < arr.length - 1 ? '\n' : '');
+		});
+		inner.append(codeFrame);
+
+		outer.append(style, overlay);
+
+		const stackDetail = createDom('details', { class: 'detail' });
+		const stackSummary = createDom('summary', {});
+		stackSummary.textContent = `${data.stack.length} stack frames were collapsed.`;
+		stackDetail.append(stackSummary);
+
+		const frames = data.stack.map(frame => {
+			const container = createDom('div', { class: 'stack-frame' });
+			const name = createDom('div', { class: 'stack-name' });
+			name.textContent = frame.name;
+			const loc = createDom('div', { class: 'stack-loc' });
+			loc.textContent = `${frame.fileName}:${frame.line}:${frame.column}`;
+
+			container.append(name, loc);
+			return container;
+		});
+		stackDetail.append(...frames);
+
+		inner.append(stackDetail);
+		iframe.contentDocument?.body.appendChild(outer);
 	});
-	close.textContent = 'close';
-	const inner = createDom('div', { class: 'wmr-error-inner' });
-	overlay.append(close, inner);
 
-	const title = createDom('h1', { class: 'wmr-error-title' });
-	title.textContent = data.error;
-	inner.append(title);
-
-	const codeFrame = createDom('pre', { class: 'wmr-error-code-frame' });
-	const code = createDom('code', {});
-	codeFrame.append(code);
-
-	data.codeFrame.split('\n').forEach((line, i, arr) => {
-		const dom = createDom('span', { class: 'wmr-error-line' + (line.startsWith('>') ? ' wmr-error-active-line' : '') });
-		dom.textContent = line;
-		code.append(dom, i < arr.length - 1 ? '\n' : '');
-	});
-	inner.append(codeFrame);
-
-	outer.append(style, overlay);
-
-	const stackDetail = createDom('details', { class: 'wmr-error-detail' });
-	const stackSummary = createDom('summary', {});
-	stackSummary.textContent = `${data.stack.length} stack frames were collapsed.`;
-	stackDetail.append(stackSummary);
-
-	const frames = data.stack.map(frame => {
-		const container = createDom('div', { class: 'wmr-error-stack-frame' });
-		const name = createDom('div', { class: 'wmr-error-stack-name' });
-		name.textContent = frame.name;
-		const loc = createDom('div', { class: 'wmr-error-stack-loc' });
-		loc.textContent = `${frame.fileName}:${frame.line}:${frame.column}`;
-
-		container.append(name, loc);
-		return container;
-	});
-	stackDetail.append(...frames);
-
-	inner.append(stackDetail);
-
-	document.body.appendChild(outer);
+	document.body.appendChild(iframe);
 }
