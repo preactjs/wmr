71beeb10b94b9606c7f316fcb3286b21489ecd6b
Marvin Hagemeister <hello@marvinh.dev>
2021-04-27 22:56:29 +0200

    Add "fixture" script for easier fixture debugging

---
+++ packages/wmr/tools/run-fixture.js
@@ -0,0 +1,15 @@
+import { spawn } from 'child_process';
+
+spawn(
+	process.execPath,
+	['src/cli.js', 'start', '--cwd', 'test/fixtures/' + process.argv.slice(2)[0], ...process.argv.slice(3)],
+	{
+		stdio: 'inherit',
+		env: {
+			// Package "application-config-path" needs this (required by devcert)
+			HOME: process.env.HOME,
+			FORCE_COLOR: '1',
+			DEBUG: 'true'
+		}
+	}
+);
