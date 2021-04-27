8e8690afee44e6d2bb83201ec3b283a89331995b
Marvin Hagemeister <hello@marvinh.dev>
2021-04-27 09:13:11 +0200

    Fix route params overwriting route properties
    When a query param was named the same as an existing key on the route
    like "path", we'd overwrite that. By moving params to their own object
    we avoid that issue all together.

---
+++ .changeset/gorgeous-scissors-compete.md
@@ -0,0 +1,5 @@
+---
+'preact-iso': major
+---
+
+Fix route params being able to overwrite route context. This is a breaking change in that params no need to be pulled off a `params` object instead of accessing it directly
--- packages/preact-iso/test/match.test.js
+++ packages/preact-iso/test/match.test.js
@@ -3,7 +3,7 @@ import { exec } from '../router.js';
 describe('match', () => {
 	it('Base route', () => {
 		const accurateResult = exec('/', '/', { path: '/' });
-		expect(accurateResult).toEqual({ path: '/' });
+		expect(accurateResult).toEqual({ path: '/', params: {} });
 
 		const inaccurateResult = exec('/user/1', '/', { path: '/' });
 		expect(inaccurateResult).toEqual(undefined);
@@ -11,7 +11,7 @@ describe('match', () => {
 
 	it('Param route', () => {
 		const accurateResult = exec('/user/2', '/user/:id', { path: '/' });
-		expect(accurateResult).toEqual({ path: '/', id: '2' });
+		expect(accurateResult).toEqual({ path: '/', params: { id: '2' } });
 
 		const inaccurateResult = exec('/', '/user/:id', { path: '/' });
 		expect(inaccurateResult).toEqual(undefined);
@@ -19,7 +19,7 @@ describe('match', () => {
 
 	it('Optional param route', () => {
 		const accurateResult = exec('/user', '/user/:id?', { path: '/' });
-		expect(accurateResult).toEqual({ path: '/' });
+		expect(accurateResult).toEqual({ path: '/', params: { id: undefined } });
 
 		const inaccurateResult = exec('/', '/user/:id?', { path: '/' });
 		expect(inaccurateResult).toEqual(undefined);
@@ -28,8 +28,18 @@ describe('match', () => {
 	it('Handles leading/trailing slashes', () => {
 		const result = exec('/about-late/_SEGMENT1_/_SEGMENT2_/', '/about-late/:seg1/:seg2/', {});
 		expect(result).toEqual({
-			seg1: '_SEGMENT1_',
-			seg2: '_SEGMENT2_'
+			params: {
+				seg1: '_SEGMENT1_',
+				seg2: '_SEGMENT2_'
+			}
+		});
+	});
+
+	it('should not overwrite existing properties', () => {
+		const result = exec('/foo/bar', '/:path/:query', { path: '/' });
+		expect(result).toEqual({
+			params: { path: 'foo', query: 'bar' },
+			path: '/'
 		});
 	});
 });
