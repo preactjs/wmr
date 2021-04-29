b8daf9475d1aa27fc6f5399975b47dbd26ab23aa
Marvin Hagemeister <hello@marvinh.dev>
2021-04-29 22:34:06 +0200

    Types: Run prettier on router.d.ts

--- packages/preact-iso/router.d.ts
+++ packages/preact-iso/router.d.ts
@@ -2,7 +2,11 @@ import { AnyComponent, FunctionComponent, VNode } from 'preact';
 
 export const LocationProvider: FunctionComponent;
 
-export function Router(props: { onLoadEnd?: (url: string) => void; onLoadStart?: (url: string) => void; children?: VNode[] }): VNode;
+export function Router(props: {
+	onLoadEnd?: (url: string) => void;
+	onLoadStart?: (url: string) => void;
+	children?: VNode[];
+}): VNode;
 
 interface LocationHook {
 	url: string;
@@ -20,7 +24,7 @@ interface RoutableProps {
 }
 
 export interface RouteProps<Props> extends RoutableProps {
-	  component: AnyComponent<Props>;
+	component: AnyComponent<Props>;
 }
 
 export function Route<Props>(props: RouteProps<Props> & Partial<Props>): VNode;
