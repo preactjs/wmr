24f9c5146ca2f04217d14155129257b857931a84
Marvin Hagemeister <hello@marvinh.dev>
2021-04-29 22:30:42 +0200

    Types: Fix useRoute and RouteProps missing in export

--- packages/preact-iso/index.d.ts
+++ packages/preact-iso/index.d.ts
@@ -1,4 +1,4 @@
 export { default as prerender } from './prerender';
-export { Router, LocationProvider, useLocation } from './router';
+export * from './router';
 export { default as lazy, ErrorBoundary } from './lazy';
 export { default as hydrate } from './hydrate';
