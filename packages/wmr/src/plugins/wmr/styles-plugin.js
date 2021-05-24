31400fcc1e1f215cd5f86fc4ae011a62fdf6dd6a
Jason Miller <developit@users.noreply.github.com>
2021-05-24 15:09:50 -0400

    Also allow numbers after a dash (".x-1")

--- packages/wmr/src/plugins/wmr/styles-plugin.js
+++ packages/wmr/src/plugins/wmr/styles-plugin.js
@@ -186,7 +186,7 @@ export default function wmrStylesPlugin({ cwd, hot, fullPath, production, alias 
 					const matches = m.match(/^(['"]?)([^:'"]+?)\1:(.+)$/);
 					if (!matches) return;
 					if (RESERVED_WORDS.test(matches[2])) return;
-					let name = matches[2].replace(/-+([a-z])/gi, (s, c) => c.toUpperCase());
+					let name = matches[2].replace(/-+([a-z0-9])/gi, (s, c) => c.toUpperCase());
 					if (name.match(/^\d/)) name = '$' + name;
 					return name + '=' + matches[3];
 				})
