75bb05fab7d7d3b4617e1cb61346ae0f41201cda
Marvin Hagemeister <hello@marvinh.dev>
2021-06-03 09:28:08 +0200

    run-fixture: install dependencies before starting

--- packages/wmr/tools/run-fixture.js
+++ packages/wmr/tools/run-fixture.js
@@ -1,16 +1,21 @@
-import { spawn } from 'child_process';
+import { spawn, spawnSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
-spawn(
-	process.execPath,
-	['src/cli.js', 'start', '--cwd', 'test/fixtures/' + process.argv.slice(2)[0], ...process.argv.slice(3)],
-	{
-		stdio: 'inherit',
-		env: {
-			// Package "application-config-path" needs this (required by devcert)
-			HOME: process.env.HOME,
-			FORCE_COLOR: '1',
-			DEBUG: 'true',
-			TERM: 'xterm-256color'
-		}
+const fixturePath = 'test/fixtures/' + process.argv.slice(2)[0];
+const pkgJson = path.join(fixturePath, 'package.json');
+
+if (fs.existsSync(pkgJson)) {
+	spawnSync('npm', ['install', '--silent'], { cwd: fixturePath });
+}
+
+spawn(process.execPath, ['src/cli.js', 'start', '--cwd', fixturePath, ...process.argv.slice(3)], {
+	stdio: 'inherit',
+	env: {
+		// Package "application-config-path" needs this (required by devcert)
+		HOME: process.env.HOME,
+		FORCE_COLOR: '1',
+		DEBUG: 'true',
+		TERM: 'xterm-256color'
 	}
-);
+});
