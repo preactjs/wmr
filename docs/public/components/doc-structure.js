preactjs/wmr

commit 9ee653794637de25491be7c8e4e5749102e00fdc

use Jekyl-style yaml file for content ordering/collections

docs/public/components/doc-structure.js
@@ -1,61 +1,19 @@
 import content from 'content:../content/docs';
+export { content };
 
-export const docStructure = [
-	{ type: 'heading', name: 'Prologue' },
-	{ type: 'page', name: 'index' },
-	{ type: 'page', name: 'cli' },
-	{ type: 'page', name: 'configuration' },
-	{ type: 'page', name: 'plugins' },
-	{ type: 'page', name: 'prerendering' },
+const pages = new Map(
+	content.map((item, index) => {
+		if ('heading' in item) return ['', -1];
+		item.slug = `/docs/${item.name}`.replace(/\/index$/g, '');
+		return [item.name, index];
+	})
+);
 
-	{ type: 'heading', name: 'API' },
-	{ type: 'page', name: 'plugin-api' }
-];
+/** @param {string} name */
+export const getPage = name => content[pages.get(name)];
 
-/** @type {Map<string, {name: string, nav?: string, title?: string, slug: string}} */
-export const docPages = new Map();
-for (const doc of content) {
-	const route = doc.name.replace(/(^|\/)index$/g, '');
-	const slug = route ? `/docs/${route}` : '/docs';
+/** @param {string} name */
+export const getPreviousPage = name => pages[pages.get(name) - 1];
 
-	docPages.set(doc.name, {
-		...doc,
-		slug
-	});
-}
-
-/**
- * @param {string} name
- * @returns {number | null}
- */
-export function getPreviousPage(name) {
-	let idx = docStructure.findIndex(x => x.name === name) - 1;
-	if (idx < 0) return null;
-	if (docStructure[idx].type === 'heading') {
-		if (idx - 1 >= 0 && docStructure[idx - 1].type !== 'heading') {
-			idx--;
-		} else {
-			return null;
-		}
-	}
-
-	return docPages.get(docStructure[idx].name);
-}
-
-/**
- * @param {string} name
- * @returns {number | null}
- */
-export function getNextPage(name) {
-	let idx = docStructure.findIndex(x => x.name === name) + 1;
-	if (idx > docStructure.length - 1) return null;
-	if (docStructure[idx].type === 'heading') {
-		if (idx + 1 < docStructure.length && docStructure[idx + 1].type !== 'heading') {
-			idx++;
-		} else {
-			return null;
-		}
-	}
-
-	return docPages.get(docStructure[idx].name);
-}
+/** @param {string} name */
+export const getNextPage = name => pages[pages.get(name) + 1];

docs/plugins/content.js
@@ -95,19 +95,65 @@ function contentRollupPlugin({ cwd, prod, ...opts }) {
 			if (!id.startsWith('\0content:')) return;
 			id = path.resolve(cwd || '.', id.slice(9));
 			const files = (await tree(id)).filter(file => file.endsWith('.md'));
-			const data = await Promise.all(
+			const dirs = path.relative(cwd, id).split(path.sep);
+			const config = (
+				await Promise.all(
+					dirs.map(async (d, i, dirs) => {
+						try {
+							return yaml.parse(await fs.readFile(path.resolve(cwd, ...dirs.slice(0, i), '_config.yml'), 'utf-8'));
+						} catch (e) {}
+					})
+				)
+			).reduce(merge, {});
+			function merge(obj, props) {
+				if (props == null) return obj;
+				if (typeof props === 'object') {
+					for (let i in props) {
+						obj[i] = obj[i] ? merge(obj[i], props[i]) : props[i];
+					}
+					return obj;
+				}
+				return props;
+			}
+			const byName = new Map();
+			let data = await Promise.all(
 				files.map(async file => {
 					const { slug, ...meta } = await getMeta(path.resolve(id, file));
-					return { name: slug || file.replace(/\.md$/, ''), ...meta };
+					const item = { name: slug || file.replace(/\.md$/, ''), ...meta };
+					byName.set(item.name, item);
+					return item;
 				})
 			);
-			data.sort((a, b) => +new Date(b.published) - +new Date(a.published));
+			const collection = path.basename(id);
+			const cfg = config.collections[collection];
+			let sortBy = 'published';
+			if (cfg) {
+				if (cfg.order) {
+					data = cfg.order.map(item => {
+						if (typeof item === 'string') return byName.get(item);
+						return item;
+					});
+				} else if (cfg.sort_by) {
+					sortBy = cfg.sort_by;
+				}
+			}
+			// detect and convert string dates and numbers to Date and Number for sorting
+			const parse = field => {
+				let d = Date.parse(field);
+				if (!isNaN(d)) return d;
+				d = Number(field);
+				if (d == field) return d;
+				return field;
+			};
+			data.sort((a, b) => parse(b[sortBy]) - parse(a[sortBy]));
 
 			let imports = '';
 
 			const serializeItem = item => {
-				const url = 'markdown:./' + path.posix.relative(path.dirname(id), path.resolve(id, item.name)) + '.md';
-				imports += `import ${JSON.stringify(url)};\n`;
+				if (item.name) {
+					const url = 'markdown:./' + path.posix.relative(path.dirname(id), path.resolve(id, item.name)) + '.md';
+					imports += `import ${JSON.stringify(url)};\n`;
+				}
 
 				let str = '{ ';
 				for (let i in item) if (item[i] != null) str += `${i}: ${JSON.stringify(item[i])}, `;

docs/public/components/sidebar.js
@@ -1,24 +1,22 @@
 import { useLocation } from 'preact-iso';
 import { SlotContent } from '../lib/slots.js';
-import { docPages, docStructure } from './doc-structure.js';
+import { content } from './doc-structure.js';
 
 export default function Sidebar() {
 	const { path } = useLocation();
 
 	return (
 		<SlotContent name="sidebar">
-			<aside class="sidebar" tabIndex="0">
+			<aside class="sidebar" tabIndex={0}>
 				<nav aria-label="secondary">
-					{docStructure.map(item => {
-						if (item.type === 'heading') {
-							return <h3 class="sidebar-title">{item.name}</h3>;
+					{content.map(item => {
+						if (item.heading) {
+							return <h3 class="sidebar-title">{item.heading}</h3>;
 						}
 
-						const page = docPages.get(item.name);
-
 						return (
-							<a key={page.name} href={page.slug} class="sidebar-nav-link" data-active={page.slug === path}>
-								{page.nav || page.title}
+							<a key={item.name} href={item.slug} class="sidebar-nav-link" data-active={item.slug === path}>
+								{item.nav || item.title}
 							</a>
 						);
 					})}

docs/public/content/_config.yml
@@ -1,0 +1,12 @@
+# https://jekyllrb.com/docs/collections/#manually-ordering-documents
+collections:
+  docs:
+    order:
+      - { heading: 'Prologue' }
+      - index
+      - cli
+      - configuration
+      - plugins
+      - prerendering
+      - { heading: 'API' }
+      - plugin-api

commit 4735d277198cf67260331519ddc146dff61bf155

Docs: Fix missing pagination
This fixes a regression introduced with 9ee653794637de25491be7c8e4e5749102e00fdc

docs/public/components/doc-structure.js
@@ -13,7 +13,7 @@ const pages = new Map(
 export const getPage = name => content[pages.get(name)];
 
 /** @param {string} name */
-export const getPreviousPage = name => pages[pages.get(name) - 1];
+export const getPreviousPage = name => content[pages.get(name) - 1];
 
 /** @param {string} name */
-export const getNextPage = name => pages[pages.get(name) + 1];
+export const getNextPage = name => content[pages.get(name) + 1];