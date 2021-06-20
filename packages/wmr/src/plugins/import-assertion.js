1e2e89fedc333dc2e74adb9f0c69320d6d46130f
Marvin Hagemeister <hello@marvinh.dev>
2021-06-20 02:45:21 +0200

    Fix node 12 unable to use named imports for commonjs

--- packages/wmr/src/plugins/import-assertion.js
+++ packages/wmr/src/plugins/import-assertion.js
@@ -1,5 +1,9 @@
-import { importAssertions } from 'acorn-import-assertions';
+import acornImportAssertions from 'acorn-import-assertions';
 import { transform } from '../lib/acorn-traverse.js';
+
+// TODO: WOrkaround for node 12 not allowing to destructure
+// CJS imports. PR an esm target for `acorn-import-assertions`.
+const { importAssertions } = acornImportAssertions;
 
 /**
  * Rewrite import assertions to url prefixes. This is done because
