5e7e4c1bb2624ef8480a05ec0cc215eaa3852383
Jovi De Croock <decroockjovi@gmail.com>
2021-08-09 16:48:03 +0200

    (wmr/hmr) - implement focus-based reconnect (#786)
    * implement focus-based reconnect
    
    * Update client.js
    
    * Create brown-gorillas-notice.md

--- packages/wmr/src/plugins/wmr/client.js
+++ packages/wmr/src/plugins/wmr/client.js
@@ -7,7 +7,7 @@ const strip = url => url.replace(/[?&]t=\d+/g, '');
 const addTimestamp = (url, time) => url + (/\?/.test(url) ? '&' : '?') + 't=' + time;
 
 const resolve = url => new URL(url, location.origin).href;
-let ws, connectTimer, connectDelay = 0;
+let ws, visibilityCallback, connectTimer, connectDelay = 0;
 
 /**
  * @param {boolean} [needsReload] Force page to reload once it's connected
@@ -23,6 +23,14 @@ function connect(needsReload) {
 	ws.addEventListener('open', () => {
 		connectDelay = 0;
 		log(`Connected to server.`);
+		if (visibilityCallback) {
+			window.removeEventListener('visibilitychange', visibilityCallback);
+			visibilityCallback = undefined;
+		}
+		if (connectTimer) {
+			clearTimeout(connectTimer);
+			connectTimer = undefined;
+		}
 		if (needsReload) {
 			window.location.reload();
 		} else {
@@ -42,6 +50,14 @@ function reconnect() {
 		if (ws) ws.close();
 		connect(false);
 	}, connectDelay);
+
+	if (!visibilityCallback) {
+		visibilityCallback = () => {
+			if (document.visibilityState === 'visible')
+				connect(false);
+		};
+	}
+	window.addEventListener('visibilitychange', visibilityCallback)
 }
 
 connect();
