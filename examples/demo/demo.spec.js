b6b57eb507296d27fd79a155174ffb66c731a032
Peter Bengtsson <mail@peterbe.com>
2021-08-25 13:12:51 -0400

    playwright test the demo

---
+++ examples/demo/demo.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test of home page', async ({ page }) => {
+	await page.goto('http://localhost:8080/');
+	await expect(page.locator('text=This is the home page').first()).toBeVisible();
+	await expect(page.locator('text=Count: 0').first()).toBeVisible();
+	await page.click('text=+');
+	await expect(page.locator('text=Count: 1').first()).toBeVisible();
+});
+
+test('basic test of navigating to About page', async ({ page }) => {
+	await page.goto('http://localhost:8080/');
+	await page.click('text=About');
+	expect(page.url()).toBe('http://localhost:8080/about');
+});
+
+test('basic test of navigating to Error page', async ({ page }) => {
+	await page.goto('http://localhost:8080/');
+	await page.click('text=Error');
+	expect(page.url()).toBe('http://localhost:8080/error');
+	await expect(page.locator("text=It's gone :(").first()).toBeVisible();
+});
