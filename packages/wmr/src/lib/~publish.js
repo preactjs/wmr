c664c12f84fbfc1bd834f62a7fd6c0d64000c313
Marvin Hagemeister <hello@marvinh.dev>
2021-09-05 11:53:27 +0200

    Add missing license and description fields

--- packages/wmr/src/lib/~publish.js
+++ packages/wmr/src/lib/~publish.js
@@ -9,14 +9,31 @@ const copy = dry ? (f, t) => console.info(`copy ${f} to ${t}`) : copyFileSync;
 
 const pkg = JSON.parse(read('package.json'));
 copy('package.json', '.package.json');
-const { name, version, bin, type, author, contributors, repository, dependencies, types, files, engines, main } = pkg;
+const {
+	name,
+	version,
+	bin,
+	type,
+	author,
+	contributors,
+	repository,
+	dependencies,
+	types,
+	files,
+	engines,
+	main,
+	license,
+	description
+} = pkg;
 const normalized = {
 	name,
 	version,
 	bin,
 	type,
 	main,
+	description,
 	author,
+	license,
 	contributors,
 	repository,
 	dependencies,
