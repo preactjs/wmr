b20f9d887225afb5369a9b09285de588be506900
Jovi De Croock <decroockjovi@gmail.com>
2021-09-25 15:03:32 +0200

    fix missing require.extensions

--- packages/wmr/src/plugins/tsconfig-paths-plugin.js
+++ packages/wmr/src/plugins/tsconfig-paths-plugin.js
@@ -3,7 +3,7 @@ import { loadConfig, createMatchPathAsync } from 'tsconfig-paths';
 export default function tsConfigPathsPlugin({ cwd }) {
 	const tsconfig = loadConfig(cwd);
 
-	if (tsconfig.resultType === 'failed') {
+	if (tsconfig.resultType === 'failed' || !tsconfig.paths) {
 		return {};
 	}
 
@@ -12,7 +12,7 @@ export default function tsConfigPathsPlugin({ cwd }) {
 	return {
 		resolveId(id) {
 			return new Promise((resolve, reject) => {
-				matchPath(id, undefined, undefined, undefined, (err, resolved) => {
+				matchPath(id, undefined, undefined, ['js', 'ts', 'tsx', 'jsx'], (err, resolved) => {
 					if (err) reject(err);
 					else resolve(resolved);
 				});
