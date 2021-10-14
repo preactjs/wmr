fcc01c942d8ee48557e40694cbf0e74db07e470d
Jason Miller <developit@users.noreply.github.com>
2021-10-14 11:50:59 -0400

    [iso] Add async onBeforeRoute() prop hook
    This implements `<Router onBeforeRoute={async url => {}}>`. When onBeforeRoute returns a Promise for a given URL transition, that Promise is thrown from within the incoming route to simulate a Suspense-based paused transition. This means each router maintains its own onBeforeRoute "locking" state.

--- packages/preact-iso/router.js
+++ packages/preact-iso/router.js
@@ -108,7 +108,9 @@ export function Router(props) {
 	// was the most recent render successful (did not suspend):
 	const didSuspend = useRef();
 	didSuspend.current = false;
-
+	// current return value of onBeforeRoute() prop
+	const onBeforeRoute = useRef();
+	
 	cur.current = useMemo(() => {
 		// This hack prevents Preact from diffing when we swap `cur` to `prev`:
 		if (this.__v && this.__v.__k) this.__v.__k.reverse();
@@ -116,6 +118,11 @@ export function Router(props) {
 		count.current++;
 
 		prev.current = cur.current;
+		
+		let obr = props.onBeforeRoute && props.onBeforeRoute(url);
+		onBeforeRoute.current = obr && obr.then && obr.then(() => {
+			if (onBeforeRoute.current === obr) onBeforeRoute.current = null;
+		});
 
 		let p, d, m;
 		toChildArray(props.children).some(vnode => {
@@ -188,11 +195,14 @@ export function Router(props) {
 	}, [path, wasPush, c]);
 
 	// Note: curChildren MUST render first in order to set didSuspend & prev.
-	return [h(RenderRef, { r: cur }), h(RenderRef, { r: prev })];
+	return [h(RenderRef, { p: onBeforeRoute.current, r: cur }), h(RenderRef, { r: prev })];
 }
 
 // Lazily render a ref's current value:
-const RenderRef = ({ r }) => r.current;
+const RenderRef = ({ p, r }) => {
+	if (p && p.then) throw p;
+	return r.current;
+};
 
 Router.Provider = LocationProvider;
 
